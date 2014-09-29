7bbaf1eefb46381d295b987ba395cc415e2f1559
Kouga <ncwhale@gmail.com>
2014-09-29 12:46:10 +0800

    Changed register form.

--- public/coffee/register.coffee
+++ public/coffee/register.coffee
@@ -5,7 +5,7 @@ requirejs ['jquery', 'bootstrap','bootstrap_validator'], ($, bs, bv)->
       submitButtons: 'button[type="submit"]'
       trigger: null
       feedbackIcons:
-        valid: 'glyphicon glyphicon-ok'
-        invalid: 'glyphicon glyphicon-remove'
-        validating: 'glyphicon glyphicon-refresh'
+        valid: 'fa fa-check-circle'
+        invalid: 'fa fa-times-circle'
+        validating: 'fa fa-star fa-spin'
   
--- public/js/register.js
+++ public/js/register.js
@@ -5,9 +5,9 @@ requirejs(['jquery', 'bootstrap', 'bootstrap_validator'], function($, bs, bv) {
       submitButtons: 'button[type="submit"]',
       trigger: null,
       feedbackIcons: {
-        valid: 'glyphicon glyphicon-ok',
-        invalid: 'glyphicon glyphicon-remove',
-        validating: 'glyphicon glyphicon-refresh'
+        valid: 'fa fa-check-circle',
+        invalid: 'fa fa-times-circle',
+        validating: 'fa fa-star fa-spin'
       }
     });
   });
